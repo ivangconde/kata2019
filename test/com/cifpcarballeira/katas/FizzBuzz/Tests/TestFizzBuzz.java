4f93c762c37845c445a04ceeced7e75ce64196a4
Ivan <ivan@acarballeira.local>
2019-02-20 14:08:15 +0100

    contiene 5 devuelve Buzz

--- test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
@@ -52,4 +52,11 @@ public class TestFizzBuzz {
         String result = fb.getFizzBuzz(15);
         assertEquals("FizzBuzz", result);
      }
+     
+     @Test
+     public void testContiene5devuelveBuzz() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.getFizzBuzz(52);
+        assertEquals("Buzz", result);
+     }
 }
