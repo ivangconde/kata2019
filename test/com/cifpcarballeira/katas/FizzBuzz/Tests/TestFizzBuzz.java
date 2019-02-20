30dc87683c7d54c2873d745afb4a9d18d9eb07e7
Julio Mosquera <julio.mosquera@yahoo.es>
2019-02-20 10:29:37 +0100

    1 devuelve 1

---
+++ test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cifpcarballeira.katas.FizzBuzz.Tests;
+
+import com.cifpcarballeira.katas.FizzBuzz.FizzBuzz;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mosquera
+ */
+public class TestFizzBuzz {
+    
+    public TestFizzBuzz() {
+    }
+
+     @Test
+     public void test1devuelve1() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.getFizzBuzz(1);
+        assertEquals("1", result);
+     }
+}
