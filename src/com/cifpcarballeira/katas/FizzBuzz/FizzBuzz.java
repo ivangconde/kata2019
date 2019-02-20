b514f91f147857a583679a2f2738759541b9cecf
Julio Mosquera <julio.mosquera@yahoo.es>
2019-02-20 12:39:48 +0100

    multiplo 3 y 5 devuelve FizzBuzz

--- src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -12,6 +12,7 @@ package com.cifpcarballeira.katas.FizzBuzz;
 public class FizzBuzz {
 
     public String getFizzBuzz(int num) {
+        if( num%3==0 && num%5==0) return "FizzBuzz";
         if( num%3 == 0 ) return "Fizz";
         if (num%5 == 0) return "Buzz";
         else return Integer.toString(num);
--- test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
@@ -45,4 +45,11 @@ public class TestFizzBuzz {
         String result = fb.getFizzBuzz(5);
         assertEquals("Buzz", result);
      }
+     
+     @Test
+     public void testMultiplo3y5devuelveFizzBuzz() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.getFizzBuzz(15);
+        assertEquals("FizzBuzz", result);
+     }
 }
